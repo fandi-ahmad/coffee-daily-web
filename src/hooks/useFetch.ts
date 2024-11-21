128d6f58f6e193982e54eb3369e26b0a15cdddd1
fandi-ahmad <fandi4160@gmail.com>
2024-11-21 19:15:43 +0800

    add hooks/usefetch

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { ApiType } from "../interface/ApiType";
+
+const useFetch = (url: string) => {
+  const [data, setData] = useState<any>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string>('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url)
+        if (!response.ok) {
+          throw new Error('Network response was not ok!')
+        }
+        const result = await response.json()
+        setData(result)
+      } catch (error: any) {
+        setError(error.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    return () => {
+      fetchData()
+    }
+  }, [url])
+
+  return { data, loading, error }
+}
+
+export default useFetch
